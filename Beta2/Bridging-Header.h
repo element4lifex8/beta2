93f680d4155629958ec0de0608406074301b09d2
Home <jasonjohnston@mojorisin.home>
2016-01-12 19:44:22 -0500

    Facebook login first cut. Changes made in "test pull" project.

---
+++ Beta2/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Beta2
+//
+//  Created by Jason Johnston on 1/2/16.
+//  Copyright © 2016 anuJ. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
