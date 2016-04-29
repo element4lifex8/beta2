1da67de6cf9334b66569034ac739ef4da9db1466
Jason Johnston <jasonjohnston@MojoRisin.home>
2016-04-28 20:03:53 -0400

    Added new assets for home screen background and updated home screen with backgound in image view Check in View logic updated so that category and city fields are always stored with their data nested a level beneath their respective nodes

--- Beta2/CheckInView.swift
+++ Beta2/CheckInView.swift
@@ -13,8 +13,8 @@ class CheckInView: UIView {
 
     @IBOutlet weak var CheckInRestField: UITextField!
     
-
-    
+    var dictArr = [[String:String]]()
+    var placesDict = [String : String]()
     
     var placesArr = [String]()
     var arrSize = Int()
@@ -45,23 +45,20 @@ class CheckInView: UIView {
         }
     }
     
-    var dictArr = [[String:String]]()
-    var categoryDict = ["category" : "true"]
-      @IBAction func categorySelect(sender: UIButton) {
-        //check to make sure category has not alreaddy been selected
-        //if(dictArr.contains($0.values.contains(sender.currentTitle!)))
-        categoryDict = ["category" : sender.currentTitle!]
+    
+    @IBAction func categorySelect(sender: UIButton) {
+         let categoryDict = ["category" : sender.currentTitle!]
         //if dictArr does not contain the selected category already then add it to the dictArr
         //uses closure for contains func as described here: http://stackoverflow.com/questions/34081580/array-of-any-and-contains
         if(!dictArr.contains({element in return (element == categoryDict)}))
         {
-            dictArr.append(categoryDict)}
+            dictArr.append(categoryDict)
         }
+    }
     
     //var cityDict = ["city" : "true"]
-    var cityDict = [String: String]()
     @IBAction func citySelect(sender: UIButton) {
-        cityDict = ["city" : sender.currentTitle!]
+        let cityDict = ["city" : sender.currentTitle!]
         //Prevent from being able to add the same city twice to the dictArr
         if(!dictArr.contains({element in return (element == cityDict)}))
         {
@@ -79,27 +76,42 @@ class CheckInView: UIView {
                 // Create a reference to a Firebase location
             let refChecked = Firebase(url:"https://check-inout.firebaseio.com/checked/\(self.currUser)")
             let refCheckedPlaces = Firebase(url:"https://check-inout.firebaseio.com/checked/places")
+            var refCheckedPlaceCat : Firebase
             // Write establishment name to user's collection
             refChecked.updateChildValues([restNameText:true])
             // Write establishment name to places collection
             refCheckedPlaces.updateChildValues([restNameText:true])
             //let userRef = refChecked.childByAppendingPath(restNameText)
             var keys = [String]()
-            
+            var overwriteFlag = false   //overwrite flag is set true the second time a key appears
             //update "user places" to contain the establishment and its categories
             for i in 0 ..< dictArr.count
             {
                 for (key,value) in dictArr[i]
                 {
-                    if keys.contains(key)  //Store children of existing entry
-                    {
+//                    if keys.contains(key)  //Store children of existing entry
+//                    {
+//                        if(overwriteFlag)  //flag is set when a new key is added to keys arrayp k
+//                        {
+//                            for j in (0 ... (i>0 ? i-1:1)).reverse() //don't overwrite first child entry when expanding child entries
+//                            {
+//                                //check if previous vlaue for matching key is nil
+//                                if let prevEntry = dictArr[j][key]
+//                                {
+//                                    RefCheckedPlaces.childByAppendingPath(restNameText).childByAppendingPath(key).updateChildValues([prevEntry:"true"])
+//                                        break
+//                                }
+//                            }
+//                            overwriteFlag = false
+//                        }
                         refCheckedPlaces.childByAppendingPath(restNameText).childByAppendingPath(key).updateChildValues([value:"true"])
-                    }
-                    else    //Create new child under the establishment's name
-                    {
-                        keys.append(key)
-                        refCheckedPlaces.childByAppendingPath(restNameText).updateChildValues([key:value])
-                    }
+//                    }
+//                    else    //Create new child under the establishment's name
+//                    {
+//                        keys.append(key)
+//                        overwriteFlag=true
+//                        refCheckedPlaces.childByAppendingPath(restNameText).updateChildValues([key:value])
+//                    }
                 }
             }
             //Save to NSUser defaults
