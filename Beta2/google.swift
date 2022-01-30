a45d3692232ab26caf3a2632a9856d024f1cd681
Jason Johnston <element4lifex8@yahoo.com>
2022-01-29 20:05:48 -0500

    Facebook assets that won't commit through GUI

---
+++ Beta2/google.swift
@@ -0,0 +1,9 @@
+//
+//  google.swift
+//  Beta2
+//
+//  Created by Jason Johnston on 11/9/19.
+//  Copyright © 2019 anuJ. All rights reserved.
+//
+
+import Foundation
---
+++ GoogleAPIKeys.h
@@ -0,0 +1,13 @@
+//
+//  GoogleAPIKeys.h
+//  Beta2
+//
+//  Created by Jason Johnston on 11/9/19.
+//  Copyright © 2019 anuJ. All rights reserved.
+//
+
+#ifndef GoogleAPIKeys_h
+#define GoogleAPIKeys_h
+
+
+#endif /* GoogleAPIKeys_h */
